79c629183fc1be38901c5fc747686ebdaed86138
zmahfuz817 <zunayed.lm@gmail.com>
2018-08-12 17:08:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  puts "The line is currently empty."
+end
+ take_a_number(katz_deli, "Ada")
+ puts ("The line is currently empty.")
+ take_a_number(katz_deli, "Grace")
+ take_a_number(katz_deli, "Kent")
+
+ line(katz_deli)
+ puts ("The line is currently empty.")
+
+ end
